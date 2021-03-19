61cdcc558cc806593e313274dd0332cda579405b
Luiz Eduardo <eduardolecdt@gmail.com>
2021-03-19 12:51:14 -0300

    VERSÃO BASE CRIADA

---
+++ assets/js/pages/index/slide-vendasClientes.js
@@ -0,0 +1,41 @@
+(function (){
+
+  // Variables
+  const section = document.querySelector('section[name="vendasClientes"]')
+  const slide = section.querySelector('div[name="slide"]')
+  const buttonAvancar = section.querySelector('button[name="avancar"]')
+  const buttonVoltar = section.querySelector('button[name="voltar"]')
+  const tamanhoElemento = 130
+  let __indice = 1
+
+  function botaoAvancarSlide () {
+    buttonAvancar.addEventListener('click', () => {
+      const numeroLinks = slide.children.length
+      if (__indice === numeroLinks) return
+
+      __indice = __indice + 1
+      rolarSlide(__indice)
+    })
+  }
+
+  function botaoVoltarSlide () {
+    buttonVoltar.addEventListener('click', () => {
+      if (__indice === 1) return
+
+      __indice = __indice - 1
+      rolarSlide(__indice)
+    })
+  }
+
+  // Auxiliares
+
+  function rolarSlide (indice) {
+    const deslocamento = tamanhoElemento * (indice - 1)
+    slide.style.transform = `translateY(-${deslocamento}px)`
+  }
+
+  // iniciar
+  botaoAvancarSlide()
+  botaoVoltarSlide()
+
+}())
